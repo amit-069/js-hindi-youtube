b0eb398c57b2c98f60048f3b29be9759be4f7c4e
amit maurya <114975661+amit-069@users.noreply.github.com>
2024-01-10 05:31:18 +0000

    dataypes

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,13 @@
+// primitive
+//7 types: String, Number ,boolean,null,undefined,symbol,BigInt
+
+
+// Reference (non primitive)
+// array,Objects,Functions
+// typeof null => object
+let scoreValue=123;
+let nulldata=null;
+let undefineddatatype=undefined;
+console.log(typeof scoreValue);
+console.log(typeof nulldata);
+console.log(typeof undefineddatatype);
