5a138c16c2a09a29fd42724e04cfab2d830eb480
amit maurya <114975661+amit-069@users.noreply.github.com>
2024-01-12 10:33:32 +0000

    dates

---
+++ 01_basic/date.js
@@ -0,0 +1,14 @@
+//Date
+
+// let myDate=new Date();
+// console.log(typeof myDate);
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+
+
+let myCretedDate=new Date(2024,0,30);
+console.log(myCretedDate.toDateString());
