d3421ff39b3ef1e43323aeb0a15c796a98f4aa63
amit maurya <114975661+amit-069@users.noreply.github.com>
2024-01-12 10:01:30 +0000

    math

---
+++ 01_basic/nums_math.js
@@ -0,0 +1,34 @@
+// const score=400;
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3));
+
+// const otherNumber=123.985
+// console.log(otherNumber.toPrecision(4));
+
+// const hundreds=10000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//+++++++++++++++++++++++Maths++++++++++++++++++++++++
+
+console.log(Math);
+// console.log(Math.abs(-7));
+// console.log(Math.abs(7));
+// console.log(Math.round(5.8));
+// console.log(Math.ceil(5.3));
+// console.log(Math.floor(5.3));
+// console.log(Math.min(3,4,8));
+// console.log(Math.max(7,9,8,5));
+
+// console.log(Math.random());
+// console.log((Math.random()*10)+1);
+// console.log(Math.floor(Math.random()*10)+1);
+ 
+const min=10;
+const max=20;
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
