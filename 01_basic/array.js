22d46e57416fcc358ccda61281b7388a3e4de829
amit maurya <114975661+amit-069@users.noreply.github.com>
2024-01-15 09:29:41 +0000

    array

---
+++ 01_basic/array.js
@@ -0,0 +1,23 @@
+//array
+const myarr=new Array(1,3,4,5);
+// console.log(myarr[0]);
+
+// //Array methods
+// myarr.push(9);
+// console.log(myarr);
+// myarr.pop();
+// console.log(myarr);
+console.log(myarr);
+// myarr.unshift(9);
+
+// console.log(myarr);
+// myarr.shift();
+
+// const myarr2=[8,9,6];
+// myarr.push(myarr2);
+
+// console.log(myarr);
+console.log(Array.isArray("Amit"))
+console.log(Array.from("Amit"))
+console.log(Array.from({name:"amit"}));
+
