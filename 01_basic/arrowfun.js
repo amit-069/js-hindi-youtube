5305a62ee6187d255faf7663057bb31aa458f53f
amit maurya <114975661+amit-069@users.noreply.github.com>
2024-01-19 10:53:08 +0000

    arrowfunction

---
+++ 01_basic/arrowfun.js
@@ -0,0 +1,24 @@
+// const user={
+//     username:"amit",
+//     price:999,
+    
+
+//     welcomeMessage:function(){
+//         console.log(`${this.username},welcome to website`);
+//         console.log(this);
+
+//     }
+// }
+
+///arrow function
+
+const addTwo=(num1,num2)=>{
+    return num1+num2;
+}
+
+console.log(addTwo(8,9));
+const printname=()=>{
+     return "Amit Maurya";
+}
+
+console.log(printname());
