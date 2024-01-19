ab3ba54d0568b9035829054c353aa65ebc970368
amit maurya <114975661+amit-069@users.noreply.github.com>
2024-01-19 10:17:03 +0000

    scope

---
+++ 01_basic/scope.js
@@ -0,0 +1,30 @@
+// if(true){
+//     let a=30;
+//     var b=20;
+// }
+
+// let a=80;
+// const b=30;
+// console.log(a);
+// console.log(b);
+
+// function one(){
+//     const username="Amit";
+//     function two(){
+//         const website="www.amit.com";
+//         console.log(username);
+//     }
+//     two()
+// }
+
+// one()
+if(true){
+    const username="Amit";
+    if(username==="Amit"){
+        const website="maurya";
+        console.log(username+website);
+
+    }
+    //console.log(website);
+}
+//console.log(username);
