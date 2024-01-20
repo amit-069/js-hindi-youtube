f7549a00f1413750d59669fe108adb894d339fc3
amit maurya <114975661+amit-069@users.noreply.github.com>
2024-01-20 10:19:35 +0000

    controlflow

---
+++ 01_basic/controlflow.js
@@ -0,0 +1,34 @@
+//if
+//  if(2=="2"){
+//     console.log("  Executed");
+//  }
+ 
+
+ // <,>,<=,>=,==(only check value,it does not check datatype),===(it check both value as well as data type),!=,!==
+
+
+//  const score=200;
+
+//  if(score>100){
+//     const power="fly";
+//     console.log(`User power:${power}`);
+//  }
+//  console.log(`User power:${power}`);
+
+// const userLoggedIn=true;
+// const debitCard=true;
+// if(userLoggedIn && debitCard){
+//     console.log("Allow to buy course");
+// }
+
+// falsy values
+// false,0,-0,BigInt 0n,"",null,undefined,NaN
+//truthy vlaues
+// "0",'false'," ",{},[], function(){}
+
+//Terniarry Operators
+
+// condition ? true:false
+
+const iceTeaPrice=100
+iceTeaPrice>=80 ? console.log("price is greater than 80"):console.log("price is less than 80");
